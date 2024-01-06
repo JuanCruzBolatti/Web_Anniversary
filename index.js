157d31742d5f00ffdbf799208449bfef10ef6996
JuanC <jbolattitorcasio@frba.utn.edu.ar>
2024-01-06 04:15:51 -0300

    first commit

--- index.js
+++ index.js
@@ -11,58 +11,30 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 /* Routing */
+app.get('/nosotros', function (req, res) {
+    res.render('about', { 
+        title: "Sobre nosotros",
+    });
+});
+app.get('/poemario/improvisacion_de_un_querer', function (req, res) {
+    res.render('poemario', { 
+        title: "Poemario - Improvisacion de un Querer",
+        tituloPoemario: "Improvisacion de un Querer",
+    });
+});
+app.get('/cositas', function (req, res) {
+    res.render('cositas', { 
+        title: "Poemario - Cositas",
+    });
+});
 app.get('/', function (req, res) {
     res.render('home', { 
-        title: "Maru Bolatti - Producción y Dirección Nacional", 
-        descrip: "Lorem xyz", 
-        robots: "index, follow" 
-    });
-});
-app.get('/conoceme', function (req, res) {
-    res.render('about', { 
-        title: "Sobre mi - Maru Bolatti", 
-        descrip: "Lorem xyz", 
-        robots: "noindex, follow" 
-    });
-});
-app.get('/direccion', function (req, res) {
-    res.render('director', { 
-        title: "Dirección Nacional - Maru Bolatti", 
-        descrip: "Lorem xyz", 
-        robots: "noindex, follow" 
-    });
-});
-app.get('/produccion', function (req, res) {
-    res.render('producer', { 
-        title: "Producción - Maru Bolatti ", 
-        descrip: "Lorem xyz", 
-        robots: "noindex, follow" 
-    });
-});
-app.get('/contacto', function (req, res) {
-    res.render('contact', { 
-        title: "Contacto - Maru Bolatti", 
-        descrip: "Lorem xyz", 
-        robots: "noindex, follow" 
+        title: "Aniversario",
     });
 });
 
-
-/* For errors */ 
-// 404 catch-all handler
-app.use(function (req, res) {
-    res.status(404);
-    res.render('404');
+/* Port in console */
+app.listen(app.get('port'), function(){
+    console.log('Started on http://localhost:' +
+    app.get('port'));
 });
-// 500 error handler
-app.use(function (err, req, res) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
-
-// /* Port in console */
-// app.listen(app.get('port'), function(){
-//     console.log('Started on http://localhost:' +
-//     app.get('port'));
-// });
