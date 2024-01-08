1a968c2b9340b3645ab6a9bc9a58213cfa168efa
JuanC <jbolattitorcasio@frba.utn.edu.ar>
2024-01-08 02:16:18 -0300

    Formato poemarios

--- index.js
+++ index.js
@@ -1,40 +1,67 @@
 var express = require('express');
 var app = express();
+const path = require('path');
+const fs = require('fs');
 
-/* Set up handlebars view engine */ 
+/* Set up handlebars view engine */
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-/* Set Port */ 
+/* Set Port */
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 /* Routing */
 app.get('/nosotros', function (req, res) {
-    res.render('about', { 
+    res.render('about', {
         title: "Sobre nosotros",
     });
 });
-app.get('/poemario/improvisacion_de_un_querer', function (req, res) {
-    res.render('poemario', { 
-        title: "Poemario - Improvisacion de un Querer",
-        tituloPoemario: "Improvisacion de un Querer",
+app.get('/poemario/:id', function (req, res) {
+    const poemariosPath = path.join(__dirname, 'data', 'poemas.json');
+
+    fs.readFile(poemariosPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error interno del servidor');
+            return;
+        }
+
+        const poemarios = JSON.parse(data);
+        const poema = poemarios.poemarios.find(p => p.id === req.params.id);
+
+        if (!poema) {
+            res.status(404).send('Libro no encontrado');
+            return;
+        }
+
+        res.render('poemario', {
+            title: "Poemario - " + poema.tituloPoemario,
+            tituloPoemario: poema.tituloPoemario,
+            tag: poema.tag,
+            linkVideo: poema.link.linkVideo,
+            linkFoto: poema.link.linkFoto,
+            linkAudio: poema.link.linkAudio,
+            explicacion: poema.explicacion,
+            poemas: poema.poemas,
+        });
     });
 });
+
 app.get('/cositas', function (req, res) {
-    res.render('cositas', { 
+    res.render('cositas', {
         title: "Poemario - Cositas",
     });
 });
 app.get('/', function (req, res) {
-    res.render('home', { 
+    res.render('home', {
         title: "Aniversario",
     });
 });
 
 /* Port in console */
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log('Started on http://localhost:' +
-    app.get('port'));
+        app.get('port'));
 });
--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,3 +1,66 @@
-function randomPoem() {
-    console.log("pepe")
+function obtenerEnteroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+let numeroEnteroAleatorio = obtenerEnteroAleatorio(1, 100);
+console.log(numeroEnteroAleatorio);
+
+function randomPoem() {
+  const poemas = ['improvisacion_de_un_querer', 'cuna_de_luna', 'estacion', 'rezos', 'improvisacion_de_un_querer_rehecho', 'improvisacion_de_un_amar', 'cuna_de_luna_rehecho', 'canto_de_luna', 'constelaciones'];
+  window.location.href = 'http://localhost:3000/poemario/' + poemas[obtenerEnteroAleatorio(0, 8)];
+}
+
+if (document.getElementById("contador")) {
+
+  // Set the date we're counting down to
+  var countDownDate = new Date("Jan 4, 2025 12:00:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("contador").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("contador").innerHTML = "FELIZ ANIVERSARIO";
+    }
+  }, 1000);
+
+}
+
+//
+function getScrollYOffset(element) {
+  var rect = element.getBoundingClientRect();
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return rect.top + scrollTop;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var finExplicacion = document.getElementById("finExplicacion");
+  const video = document.getElementById("youtubePlayer");
+
+  window.onscroll = () => {
+    if(window.scrollY > getScrollYOffset(finExplicacion)) {
+      video.classList.add("video-chiquito");
+    } else {
+      video.classList.remove("video-chiquito");
+    }
+  }
+});
+
+
